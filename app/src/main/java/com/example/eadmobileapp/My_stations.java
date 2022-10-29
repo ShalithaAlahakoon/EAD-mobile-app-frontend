9fae5deefcf02f846330e8397bd6764176848685
ShalihtaAlahakoon <it19960432@my.sliit.lk>
2022-10-29 08:55:00 +0530

    my stations

---
+++ app/src/main/java/com/example/eadmobileapp/My_stations.java
@@ -0,0 +1,14 @@
+package com.example.eadmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My_stations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_stations);
+    }
+}
